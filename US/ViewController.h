fd245dee6c7b12dcd2ee9cb04cf836be5aaa04c2
NingFangming <Fangming@NingFangmings-MacBook-Pro.local>
2015-08-06 22:22:35 -0400

    login page

--- US/ViewController.h
+++ US/ViewController.h
@@ -7,5 +7,11 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIImageView *usernameIcon;
+@property (weak, nonatomic) IBOutlet UIImageView *PWIcon;
+@property (weak, nonatomic) IBOutlet UITextField *usernameTextfield;
+@property (weak, nonatomic) IBOutlet UITextField *PWTextfield;
+@property (weak, nonatomic) IBOutlet UIButton *signInBotton;
+@property (weak, nonatomic) IBOutlet UIButton *signUpButton;
 
 @end
--- US/ViewController.m
+++ US/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    //background image
     UIGraphicsBeginImageContext(self.view.frame.size);
     [[UIImage imageNamed:@"loginBg.png"] drawInRect:self.view.bounds];
     UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
@@ -24,9 +25,63 @@
     
     self.view.backgroundColor = [UIColor colorWithPatternImage:image];
     
+    //login icon image
+    _usernameIcon.image=[UIImage imageNamed:@"loginUsernameIcon.png"];
+    _PWIcon.image=[UIImage imageNamed:@"loginPWIcon.png"];
+    
+    //login textfield
+    [_usernameTextfield setBackgroundColor:[UIColor clearColor]];//transparent
+    [_usernameTextfield setBorderStyle:UITextBorderStyleNone];//non-border
+    [_usernameTextfield setTextColor:[UIColor whiteColor]];//text color:white
+    [_PWTextfield setBackgroundColor:[UIColor clearColor]];
+    [_PWTextfield setBorderStyle:UITextBorderStyleNone];
+    [_PWTextfield setTextColor:[UIColor whiteColor]];
+
+    //login button color
+    
+    [_signInBotton setBackgroundColor:[self colorWithHexString:@"089DD8"]];
+    [_signUpButton setBackgroundColor:[self colorWithHexString:@"089DD8"]];
+    
+    
     PFObject *testObject = [PFObject objectWithClassName:@"TestObject"];
     testObject[@"foo"] = @"bar123";
     //[testObject saveInBackground];
+}
+
+    -(UIColor*)colorWithHexString:(NSString*)hex
+{
+    NSString *cString = [[hex stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] uppercaseString];
+    
+    // String should be 6 or 8 characters
+    if ([cString length] < 6) return [UIColor grayColor];
+    
+    // strip 0X if it appears
+    if ([cString hasPrefix:@"0X"]) cString = [cString substringFromIndex:2];
+    
+    if ([cString length] != 6) return  [UIColor grayColor];
+    
+    // Separate into r, g, b substrings
+    NSRange range;
+    range.location = 0;
+    range.length = 2;
+    NSString *rString = [cString substringWithRange:range];
+    
+    range.location = 2;
+    NSString *gString = [cString substringWithRange:range];
+    
+    range.location = 4;
+    NSString *bString = [cString substringWithRange:range];
+    
+    // Scan values
+    unsigned int r, g, b;
+    [[NSScanner scannerWithString:rString] scanHexInt:&r];
+    [[NSScanner scannerWithString:gString] scanHexInt:&g];
+    [[NSScanner scannerWithString:bString] scanHexInt:&b];
+    
+    return [UIColor colorWithRed:((float) r / 255.0f)
+                           green:((float) g / 255.0f)
+                            blue:((float) b / 255.0f)
+                           alpha:1.0f];
 }
 
 - (void)didReceiveMemoryWarning {
